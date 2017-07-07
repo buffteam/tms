217d7f935dec603f4ab0af96ba6ff975395b99e9
lin-xin <2981207131@qq.com>
2017-07-07 15:21:41 +0800

    添加我的分享列表

--- resources/views/doc/share.blade.php
+++ resources/views/doc/share.blade.php
@@ -4,68 +4,87 @@
 @endsection
 
 @section('style')
-    <link rel="stylesheet" href="{{asset('module/doc/css/myShare.css')}}">
+    <link rel="stylesheet" href="{{asset('module/doc/css/layouts/myShare.css')}}">
 @endsection
 
 @section('content')
-    <div class="layui-form">
-        <table class="layui-table">
-            <colgroup>
-                <col width="50">
-                <col width="150">
-                <col width="150">
-                <col width="200">
-                <col>
-            </colgroup>
+    <div class="table-wrapper">
+        <table class="table-list">
             <thead>
-            <tr>
-                <th><input type="checkbox" name="" lay-skin="primary" lay-filter="allChoose"></th>
-                <th>人物</th>
-                <th>民族</th>
-                <th>出场时间</th>
-                <th>格言</th>
-            </tr>
+                <tr>
+                    <th>笔记标题</th>
+                    <th>发布日期</th>
+                    <th>更新日期</th>
+                    <th width="50%">已读次数</th>
+                </tr>
             </thead>
             <tbody>
             <tr>
-                <td><input type="checkbox" name="" lay-skin="primary"></td>
                 <td>贤心</td>
                 <td>汉族</td>
                 <td>1989-10-14</td>
                 <td>人生似修行</td>
             </tr>
             <tr>
-                <td><input type="checkbox" name="" lay-skin="primary"></td>
                 <td>张爱玲</td>
                 <td>汉族</td>
                 <td>1920-09-30</td>
                 <td>于千万人之中遇见你所遇见的人，于千万年之中，时间的无涯的荒野里…</td>
             </tr>
             <tr>
-                <td><input type="checkbox" name="" lay-skin="primary"></td>
-                <td>Helen Keller</td>
-                <td>拉丁美裔</td>
-                <td>1880-06-27</td>
-                <td> Life is either a daring adventure or nothing.</td>
-            </tr>
-            <tr>
-                <td><input type="checkbox" name="" lay-skin="primary"></td>
-                <td>岳飞</td>
+                <td>贤心</td>
                 <td>汉族</td>
-                <td>1103-北宋崇宁二年</td>
-                <td>教科书再滥改，也抹不去“民族英雄”的事实</td>
+                <td>1989-10-14</td>
+                <td>人生似修行</td>
             </tr>
             <tr>
-                <td><input type="checkbox" name="" lay-skin="primary"></td>
-                <td>孟子</td>
-                <td>华夏族（汉族）</td>
-                <td>公元前-372年</td>
-                <td>猿强，则国强。国强，则猿更强！ </td>
+                <td>张爱玲</td>
+                <td>汉族</td>
+                <td>1920-09-30</td>
+                <td>于千万人之中遇见你所遇见的人，于千万年之中，时间的无涯的荒野里…</td>
+            </tr>
+            <tr>
+                <td>贤心</td>
+                <td>汉族</td>
+                <td>1989-10-14</td>
+                <td>人生似修行</td>
+            </tr>
+            <tr>
+                <td>张爱玲</td>
+                <td>汉族</td>
+                <td>1920-09-30</td>
+                <td>于千万人之中遇见你所遇见的人，于千万年之中，时间的无涯的荒野里…</td>
+            </tr>
+            <tr>
+                <td>贤心</td>
+                <td>汉族</td>
+                <td>1989-10-14</td>
+                <td>人生似修行</td>
+            </tr>
+            <tr>
+                <td>张爱玲</td>
+                <td>汉族</td>
+                <td>1920-09-30</td>
+                <td>于千万人之中遇见你所遇见的人，于千万年之中，时间的无涯的荒野里…</td>
             </tr>
             </tbody>
         </table>
     </div>
+
+    <script id="table-tpl" type="text/html">
+        <% for(var i = 0; i < list.length; i++) { %>
+        <tr>
+            <td><%= list[i].title %></td>
+            <td><%= list[i].created_at %></td>
+            <td><%= list[i].updated_at %></td>
+            <td>99</td>
+        </tr>
+        <% } %>
+    </script>
 @endsection
 @section('script')
-    <script src="{{asset('/module/doc/js/myShare.js')}}"></script>
+    {{--<script src="{{asset('/module/doc/js/myShare.js')}}"></script>--}}
+    <script>
+
+    </script>
 @endsection
