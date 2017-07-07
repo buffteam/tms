14ee0fc5d19e0e35f57bad6ddfe2fac0f468b175
“albean <ifengdb@163.com>
2017-07-07 15:27:41 +0800

    删除无用依赖

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use Knp\Snappy\Pdf;
+use Illuminate\Support\Facades\App;
 use App\Notes;
 use Dompdf\Dompdf;
 use App\Libs\upload;
@@ -44,40 +44,35 @@ class CommonController extends BaseController
         $params = $request->input();
         // instantiate and use the dompdf class
         $data = Notes::find(19);
-
-
-
-
-
-        $myProjectDirectory = '/path/to/my/project';
-
-        $snappy = new Pdf($myProjectDirectory . '/vendor/h4cc/wkhtmltopdf-i386/bin/wkhtmltopdf-i386');
-
-// or
-
-        $snappy = new Pdf($myProjectDirectory . '/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64');
-
-        $snappy = new Pdf('/path/to/binary');
-
-        $snappy->setOption('toc', true);
-        $snappy->setOption('xsl-style-sheet', 'http://path/to/stylesheet.xsl');//or local file;
-
-        $snappy->generateFromHtml('<p>Some content</p>', 'test.pdf');
-
-        $dompdf = new Dompdf();
-//        var_dump($data->content);
-//        return;
-        $dompdf->loadHtml(utf8_decode($data->content),'UTF-8');
-
-        // (Optional) Setup the paper size and orientation
-        $dompdf->setPaper('A4', 'landscape');
-
-        // Render the HTML as PDF
-        $dompdf->render();
-
-
-        // Output the generated PDF to Browser
-        $dompdf->stream($data->title);
+        $snappy = App::make('snappy.pdf');
+//To file
+        $html = $data->content;
+        $snappy->generateFromHtml($data->content, '/tmp/bill-123.pdf');
+//        $snappy->generate('http://www.github.com', '/tmp/github.pdf');
+//Or output:
+        return response(
+            $snappy->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+                'Content-Disposition'   => 'attachment; filename="file.pdf"'
+            )
+        );
+//
+//        $dompdf = new Dompdf();
+////        var_dump($data->content);
+////        return;
+//        $dompdf->loadHtml(utf8_decode($data->content),'UTF-8');
+//
+//        // (Optional) Setup the paper size and orientation
+//        $dompdf->setPaper('A4', 'landscape');
+//
+//        // Render the HTML as PDF
+//        $dompdf->render();
+//
+//
+//        // Output the generated PDF to Browser
+//        $dompdf->stream($data->title);
     }
     public function index ()
     {
