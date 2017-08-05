cbf5c12c32ca28e84c5801d0bc91bce57eae39c4
冯德兵 <fengdebing@oaserver.dw.gdbbk.com>
2017-08-05 11:46:47 +0800

    去掉删除的

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -62,7 +62,7 @@ class DashboardController extends BaseController
 
     public function getRankingList()
     {
-        $list = DB::select('SELECT u.name,COUNT(*) AS num FROM stip_users u RIGHT JOIN stip_notes n ON u.id = n.u_id GROUP BY u.name ORDER BY num desc limit 10');
+        $list = DB::select('SELECT u.name,COUNT(*) AS num FROM stip_users u RIGHT JOIN stip_notes n ON u.id = n.u_id where n.active = "1" GROUP BY u.name ORDER BY num desc limit 10');
         return $this->ajaxSuccess('获取榜单数据成功',$list);
     }
 
