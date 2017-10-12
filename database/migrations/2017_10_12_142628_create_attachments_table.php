8e0cb3b028f32c65845311d0a108028252e78b3a
fengdb <fengdebing@oaserver.dw.gdbbk.com>
2017-10-12 15:00:03 +0800

    增加笔记关联附件表

---
+++ database/migrations/2017_10_12_142628_create_attachments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url')->comment('附件地址');
+            $table->tinyInteger('type')->comment('附件类型');
+            $table->integer('size')->comment('附件大小单位kb');
+            $table->integer('note_id')->comment('所属笔记');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
